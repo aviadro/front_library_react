5453440c96623600af5abfe388a4bc7b603a6d74
Aviad Roichman <aviadroichman@gmail.com>
2025-01-02 15:36:41 +0200

    starting to work..

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Jumbotron from './components/Jumbotron';
+
+function App() {
+  return (
+    <>
+    <Jumbotron/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <div class="container text-center">
+        <h1>The Library</h1>      
+        <p>Loaning, Reading & Enjoing</p>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbotron
